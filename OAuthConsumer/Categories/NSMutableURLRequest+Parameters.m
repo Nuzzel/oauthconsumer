f1881a10c5e5d53376072119487063622d7f8fb1
Stephen Birarda <commit@birarda.com>
2012-11-29 14:54:32 -0500

    add continue in enumeration for HTTP body content

--- OAuthConsumer/Categories/NSMutableURLRequest+Parameters.m
+++ OAuthConsumer/Categories/NSMutableURLRequest+Parameters.m
@@ -52,6 +52,11 @@ static NSString *Boundary = @"-----------------------------------0xCoCoaouTHeBou
     NSMutableArray *requestParameters = [NSMutableArray arrayWithCapacity:[encodedParameterPairs count]];
     
     for (NSString *encodedPair in encodedParameterPairs) {
+        if ([encodedPair rangeOfString:@"="].location == NSNotFound) {
+            // this is *REQUIRED* to POST data in HTTPBody to LinkedIn API
+            continue; 
+        }
+        
         NSArray *encodedPairElements = [encodedPair componentsSeparatedByString:@"="];
         OARequestParameter *parameter = [[OARequestParameter alloc] initWithName:[[encodedPairElements objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]
                                                                            value:[[encodedPairElements objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
